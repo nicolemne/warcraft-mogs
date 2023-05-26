49d4cd5657b796d105b375f6c608e93294eb9b56
Nicole MN.E <mcnultyengstrom@gmail.com>
2023-05-26 18:53:21 +0000

    Add installed apps to settings.py

--- WC_MOGS/settings.py
+++ WC_MOGS/settings.py
@@ -40,7 +40,17 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'django_summernote',
+    'crispy_forms',
+    'crispy_bootstrap4',
+    'WC_MOGS',
 ]
 
 MIDDLEWARE = [

7d780e7b2925e8f13d03b0d0b754470e2f9bfb78
Nicole MN.E <mcnultyengstrom@gmail.com>
2023-06-06 19:41:49 +0000

    Add bootstrap html templates to repository

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
+
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(staus=1).order_by('-created_on')

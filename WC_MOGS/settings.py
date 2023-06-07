748ddf61564aa335c63246dd07c8cd11ff400cb0
Nicole MN.E <mcnultyengstrom@gmail.com>
2023-06-07 19:07:33 +0000

    Add login feature

--- WC_MOGS/settings.py
+++ WC_MOGS/settings.py
@@ -34,10 +34,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['warcraft-mogs.herokuapp.com', 'localhost', '8000-nicolemne-warcraftmogs-1sqa2tu0ftz.ws-eu98.gitpod.io']
-
-# Identify which 'site' instance is being used
-SITE_ID = 1
+ALLOWED_HOSTS = ['warcraft-mogs.herokuapp.com', 'localhost',
+                 '8000-nicolemne-warcraftmogs-1sqa2tu0ftz.ws-eu98.gitpod.io']
 
 # Application definition
 
@@ -59,6 +57,12 @@ INSTALLED_APPS = [
     'crispy_bootstrap4',
     'blog',
 ]
+
+# Identify which 'site' instance is being used
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

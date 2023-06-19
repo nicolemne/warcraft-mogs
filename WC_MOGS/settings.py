b61525d3dda80033ee6a3f3b0bc19b2d38bead8c
Nicole MN.E <mcnultyengstrom@gmail.com>
2023-06-19 18:43:03 +0000

    Debug set to False

--- WC_MOGS/settings.py
+++ WC_MOGS/settings.py
@@ -33,7 +33,7 @@ DATABASES = {
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
